71dc10175853124858272b771934278fee9701e8
Jokinko <jozefchmelar@users.noreply.github.com>
2021-06-16 10:08:04 +0200

    Updated packages Added compiler to connector for autocopy Enviroment variable for ams target

--- PlcHammerConnector/Entry.cs
+++ PlcHammerConnector/Entry.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Vortex.Adapters.Connector.Tc3.Adapter;
 
 namespace PlcHammerConnector
 {
     public class Entry
     {
-        const string AmsId = "172.20.10.2.1.1";
+        public readonly static string AmsId = Environment.GetEnvironmentVariable("Tc3Target");
 
-        public static PlcHammer.PlcHammerTwinController PlcHammer { get; } = new PlcHammer.PlcHammerTwinController(Tc3ConnectorAdapter.Create(AmsId, 851, true));
+        public static PlcHammer.PlcHammerTwinController PlcHammer { get; } 
+            = new PlcHammer.PlcHammerTwinController(Tc3ConnectorAdapter.Create(AmsId, 851, true));
     }
 }

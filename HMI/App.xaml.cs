9915249bf6ec615269c4c11da366b08b6015b69b
Peter <61538034+PTKu@users.noreply.github.com>
2021-06-22 01:36:14 -0700

    Data examples merging data samples  (#3)
    * Updated packages
    Added compiler to connector for autocopy
    Enviroment variable for ams target
    
    * Add data into the project with a tutorial
    
    * poco explanation
    
    Co-authored-by: Jokinko <jozefchmelar@users.noreply.github.com>

--- HMI/App.xaml.cs
+++ HMI/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using PlcHammerConnector;
+﻿using PlcHammer;
+using PlcHammerConnector;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -6,6 +7,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using TcOpen.Inxton.Abstractions.Data;
+using TcOpen.Inxton.MongoDb;
 
 namespace HMI
 {
@@ -14,14 +17,24 @@ namespace HMI
     /// </summary>
     public partial class App : Application
     {
+
         public App()
         {
             TcOpen.Inxton.TcoAppDomain.Current.Builder
                 .SetUpLogger(new TcOpen.Inxton.Logging.SerilogAdapter())
                 .SetDispatcher(TcoCore.Wpf.Threading.Dispatcher.Get);
 
+
+
             Entry.PlcHammer.Connector.ReadWriteCycleDelay = 200;
-            Entry.PlcHammer.Connector.BuildAndStart();               
+            Entry.PlcHammer.Connector.BuildAndStart();
+
+            var mongoUri = "mongodb://localhost:27017";
+            var databaseName = "Hammer";
+            var collectionName = "HammerCollection";
+            var mongoSettings = new MongoDbRepositorySettings<PlainStation001_ProductionData>(mongoUri,databaseName,collectionName);
+            var repository = new MongoDbRepository<PlainStation001_ProductionData>(mongoSettings);
+            Entry.PlcHammer.MAIN._app._station001._dataManager.InitializeRepository(repository);
         }
     }
 }
